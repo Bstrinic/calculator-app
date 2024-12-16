641f2992d2f83f9ce35eabb834466e4ad79e8e2d
Bstrinic <BogdanStrinic003@gmail.com>
2024-12-16 09:44:58 -0700

    tried to fix status bar showing - failed

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import ScientificCalculator from "./screens/ScientificCalculator";
 import SignIn from "./screens/SignIn";
 import SavedCalculations from "./screens/SavedCalculations";
 import MyHeader from "./components/MyHeader";
+import "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Stack = createStackNavigator();
 
@@ -18,58 +20,58 @@ export default function App() {
   const toggleTheme = () => setIsDarkMode((prev) => !prev);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="SplashScreen"
-        screenOptions={({ navigation, route }) => ({
-          header: () => (
-            <MyHeader
-              navigation={navigation}
-              title={route.name}
-              isEnabled={isDarkMode}
-              toggleSwitch={toggleTheme}
-            />
-          ),
-          headerStyle: {
-            backgroundColor: isDarkMode ? "#000" : "#fff", // Dynamic header background
-          },
-          headerTintColor: isDarkMode ? "#fff" : "#000", // Dynamic header text color
-          headerBackTitleVisible: false,
-        })}
-      >
-        {/* Splash Screen */}
-        <Stack.Screen
-          name="SplashScreen"
-          component={SplashScreen}
-          options={{ headerShown: false }}
-        />
-        {/* Basic Calculator */}
-        <Stack.Screen
-          name="BasicCalculator"
-          component={(props) => (
-            <BasicCalculator {...props} isDarkMode={isDarkMode} />
-          )}
-        />
-        {/* Scientific Calculator */}
-        <Stack.Screen
-          name="ScientificCalculator"
-          component={(props) => (
-            <ScientificCalculator {...props} isDarkMode={isDarkMode} />
-          )}
-        />
-        {/* Sign In */}
-        <Stack.Screen
-          name="SignIn"
-          component={(props) => (
-            <SignIn {...props} />)}
-        />
-        {/* Saved Calculations */}
-        <Stack.Screen
-          name="SavedCalculations"
-          component={(props) => (
-            <SavedCalculations {...props} />)}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="SplashScreen"
+          screenOptions={({ navigation, route }) => ({
+            header: () => (
+              <MyHeader
+                navigation={navigation}
+                title={route.name}
+                isEnabled={isDarkMode}
+                toggleSwitch={toggleTheme}
+              />
+            ),
+            headerStyle: {
+              backgroundColor: isDarkMode ? "#000" : "#fff", // Dynamic header background
+            },
+            headerTintColor: isDarkMode ? "#fff" : "#000", // Dynamic header text color
+            headerBackTitleVisible: false,
+          })}
+        >
+          {/* Splash Screen */}
+          <Stack.Screen
+            name="SplashScreen"
+            component={SplashScreen}
+            options={{ headerShown: false }}
+          />
+          {/* Basic Calculator */}
+          <Stack.Screen
+            name="BasicCalculator"
+            component={(props) => (
+              <BasicCalculator {...props} isDarkMode={isDarkMode} />
+            )}
+          />
+          {/* Scientific Calculator */}
+          <Stack.Screen
+            name="ScientificCalculator"
+            component={(props) => (
+              <ScientificCalculator {...props} isDarkMode={isDarkMode} />
+            )}
+          />
+          {/* Sign In */}
+          <Stack.Screen
+            name="SignIn"
+            component={(props) => <SignIn {...props} />}
+          />
+          {/* Saved Calculations */}
+          <Stack.Screen
+            name="SavedCalculations"
+            component={(props) => <SavedCalculations {...props} />}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
